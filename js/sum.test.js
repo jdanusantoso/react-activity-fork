6c3265e1cefeab09d30ca1d85ded4528e5a58e67
Johannes Danusantoso <jdanusantoso@hotmail.com>
2023-02-11 15:02:03 -0600

    adding negative numbers

--- js/sum.test.js
+++ js/sum.test.js
@@ -7,3 +7,7 @@ test('properly adds two numbers', () => {
 test('properly adds two numbers', () => {
     expect(sum(3,9)).toBe(12)
 })
+
+test('properly adds two numbers', () => {
+    expect(sum(-3,-9)).toBe(-12)
+})

427887c462540e21d952f658fea880d9adfdbe57
Johannes Danusantoso <jdanusantoso@hotmail.com>
2023-02-11 14:59:20 -0600

    subtract test completed

--- js/subtract.test.js
+++ js/subtract.test.js
@@ -1,9 +1,13 @@
-const sum = require('./sum')
+const subtract = require('./subtract')
 
-test('properly adds two numbers', () => {
-    expect(sum(1,2)).toBe(3)
+test('properly subtract two numbers', () => {
+    expect(subtract(4, 1)).toBe(3)
 })
 
-test('properly adds two numbers', () => {
-    expect(sum(3,9)).toBe(12)
+test('properly subtract two numbers', () => {
+    expect(subtract(9, 3)).toBe(6)
+})
+
+test('properly subtract two numbers', () => {
+    expect(subtract(9, 15)).toBe(-6)
 })

44990dc3b2e62b7c056604a35da96a9436b8fdc2
Johannes Danusantoso <jdanusantoso@hotmail.com>
2023-02-21 12:19:23 -0600

    two elements rendered on view

--- component-testing/testing1-react-app/src/components/todo.js
+++ component-testing/testing1-react-app/src/components/todo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-function todo({}) {
+//render a todo item and want user to know when it has been completed or not
+function todo({todo}) {
     return <div data-testid="todo-1">Hello World!</div>
 }
 

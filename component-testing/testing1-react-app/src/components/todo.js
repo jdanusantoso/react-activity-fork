0c757b7f5c77b4fe96914c97c79aa6a4f78dc00d
Johannes Danusantoso <jdanusantoso@hotmail.com>
2023-02-21 12:24:21 -0600

    crossing out list element

--- component-testing/testing1-react-app/src/components/todo.js
+++ component-testing/testing1-react-app/src/components/todo.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 //render a todo item and want user to know when it has been completed or not
 function todo({todo}) {
-    return <div data-testid="todo-1">Hello World!</div>
+    const { id, title, completed } = todo;
+    const h1 = <h1>{title}</h1>;
+    const text = completed ? <strike>{h1}</strike>:h1;
+    return <div data-testid={`todo-${id}`}>{ text }</div>
 }
 
 export default todo;

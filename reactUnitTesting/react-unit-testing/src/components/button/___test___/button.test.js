fb52aa2188c436625f358feab1a5e7f7fe92c171
Johannes Danusantoso <jdanusantoso@hotmail.com>
2023-02-21 16:51:10 -0600

    another unit test finished

--- reactUnitTesting/react-unit-testing/src/components/button/___test___/button.test.js
+++ reactUnitTesting/react-unit-testing/src/components/button/___test___/button.test.js
@@ -1,13 +1,21 @@
+/* eslint-disable testing-library/prefer-screen-queries */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from './../button';
 import { isTSAnyKeyword } from '@babel/types';
 import { render } from '@testing-library/react'
-import "jest-dom/extend-expect";
+import '@testing-library/jest-dom';
 
 it("renders without crashing", () => {
     //create an element
     const div = document.createElement("div");
     //render the element and attach it to the div
     ReactDOM.render(<Button></Button>, div)
+})
+
+it("renders button correctly", () => {
+    //create an element
+    const {getByTestId} = render(<Button label="click me please"></Button>);
+    //render the element and attach it to the div
+    expect(getByTestId('button')).toHaveTextContent("click me please");
 })

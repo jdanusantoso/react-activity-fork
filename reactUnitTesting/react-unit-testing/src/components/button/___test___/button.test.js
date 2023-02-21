9554df6fb108a63c83b77c97d102f72005061ca5
Johannes Danusantoso <jdanusantoso@hotmail.com>
2023-02-21 16:56:08 -0600

    save button test

--- reactUnitTesting/react-unit-testing/src/components/button/___test___/button.test.js
+++ reactUnitTesting/react-unit-testing/src/components/button/___test___/button.test.js
@@ -3,8 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from './../button';
 import { isTSAnyKeyword } from '@babel/types';
-import { render } from '@testing-library/react'
+import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom';
+
+afterEach(cleanup);
 
 it("renders without crashing", () => {
     //create an element
@@ -18,4 +20,12 @@ it("renders button correctly", () => {
     const {getByTestId} = render(<Button label="click me please"></Button>);
     //render the element and attach it to the div
     expect(getByTestId('button')).toHaveTextContent("click me please");
+})
+
+//Created two buttons and added to dom without cleanup
+it("renders button correctly again", () => {
+    //create an element
+    const {getByTestId} = render(<Button label="save"></Button>);
+    //render the element and attach it to the div
+    expect(getByTestId('button')).toHaveTextContent("save");
 })

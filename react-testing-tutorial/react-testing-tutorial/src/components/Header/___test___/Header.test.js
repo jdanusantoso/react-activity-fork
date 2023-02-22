56c495dd4de0aef98c3c46b0b5fcacf832369033
Johannes Danusantoso <jdanusantoso@hotmail.com>
2023-02-22 12:59:31 -0600

    first unit test -header

---
+++ react-testing-tutorial/react-testing-tutorial/src/components/Header/___test___/Header.test.js
@@ -0,0 +1,91 @@
+import { render, screen } from '@testing-library/react';
+import Header from '../Header';
+
+//FAILED TEST
+test('renders learn react link-fail', () => {
+    render(<Header title="My Header"/>);
+    const headingElement = screen.getByText(/learn react/i);
+    expect(headingElement).toBeInTheDocument();
+  });
+
+  test('renders learn react link-pass', () => {
+    render(<Header title="My Header"/>);
+    const headingElement = screen.getByText(/my header/i);
+    expect(headingElement).toBeInTheDocument();
+  });
+
+// it('should render same text passed into title prop', () => {
+//     render(
+//         <Header 
+//           title="todo"
+//         />
+//     );
+//     const h1Element = screen.getByRole("heading");
+//     expect(h1Element).toBeInTheDocument();
+// });
+
+// it('should render same text passed into title prop', () => {
+//     render(
+//         <Header 
+//           title="todo"
+//         />
+//     );
+//     const h1Element = screen.getByRole("heading", { name: /todo/i });
+//     expect(h1Element).toBeInTheDocument();
+// });
+
+// it('should render same text passed into title prop', () => {
+//     render(
+//         <Header 
+//           title="todo"
+//         />
+//     );
+//     const h1Element = screen.getByTitle("Header");
+//     expect(h1Element).toBeInTheDocument();
+// });
+
+// it('should render same text passed into title prop', () => {
+//     render(
+//         <Header 
+//           title="todo"
+//         />
+//     );
+//     const h2Element = screen.getByTestId("header-2");
+//     expect(h2Element).toBeInTheDocument();
+// });
+
+// // WITH FINDBY
+
+// it('should render same text passed into title prop', async () => {
+//     render(
+//         <Header 
+//           title="todo"
+//         />
+//     );
+//     const h1Element = await screen.findByText(/todo/i);
+//     expect(h1Element).toBeInTheDocument();
+// });
+
+// // WITH QUERYBY
+
+// it('should render same text passed into title prop', () => {
+//     render(
+//         <Header 
+//           title="todo"
+//         />
+//     );
+//     const h1Element = screen.queryByText(/dogs/i);
+//     expect(h1Element).not.toBeInTheDocument
+// });
+
+// // WITH GETALLBY
+
+// it('should render same text passed into title prop', () => {
+//     render(
+//         <Header 
+//           title="todo"
+//         />
+//     );
+//     const h1Elements = screen.getAllByText(/todo/i);
+//     expect(h1Elements.length).toBe(1);
+// });

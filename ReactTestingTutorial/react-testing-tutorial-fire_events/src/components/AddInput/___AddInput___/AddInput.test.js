50ad4f675f8e20e239a2538c8d6e48af6cd27001
Johannes Danusantoso <jdanusantoso@hotmail.com>
2023-02-23 09:48:08 -0600

    fire event successful

--- ReactTestingTutorial/react-testing-tutorial-fire_events/src/components/AddInput/___AddInput___/AddInput.test.js
+++ ReactTestingTutorial/react-testing-tutorial-fire_events/src/components/AddInput/___AddInput___/AddInput.test.js
@@ -3,6 +3,9 @@ import AddInput from "../AddInput"
 
 const mockedSetTodo = jest.fn();
 
+/*Test 1 
+Used to fetch the placeholder text
+*/
 describe("AddInput", () => {
     render(
         <AddInput 
@@ -15,5 +18,21 @@ describe("AddInput", () => {
     it('', async () => {
         const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
         expect(inputElement).toBeInTheDocument();
-    })
+    });
+
+    it ("should be able to type into input", () => {
+        render(
+            <AddInput 
+                //get the todos from state in AddInput.js
+                todos={[]}
+                //passing the mockedSetTodo into the set todos
+                setTodos={mockedSetTodo}
+            />
+        );
+
+        const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+        fireEvent.change(inputElement, { target: { value: "Go Grocery Shopping"}})
+        expect(inputElement.value).toBe("Go Grocery Shopping");
+            
+        });
 })

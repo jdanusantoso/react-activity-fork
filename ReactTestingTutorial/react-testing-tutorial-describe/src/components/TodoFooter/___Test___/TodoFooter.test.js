925d6e4ad93ac94458f94abf70b7430ca3e077d0
Johannes Danusantoso <jdanusantoso@hotmail.com>
2023-02-22 22:06:01 -0600

    describe block form 1

---
+++ ReactTestingTutorial/react-testing-tutorial-describe/src/components/TodoFooter/___Test___/TodoFooter.test.js
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react';
+import TodoFooter from '../TodoFooter';
+import { BrowserRouter } from "react-router-dom"
+
+
+const MockTodoFooter = ({numberOfIncompleteTasks}) => {
+  return (
+    <BrowserRouter>
+    numberOfIncompleteTasks={numberOfIncompleteTasks}
+    </BrowserRouter>
+  )
+}
+
+/*TEST 1*/ 
+
+//Describe puts related tests into blocks
+
+//FAILED TEST
+describe("TodoFooter", () => {
+  it('should render the correct amount of incomplete tasks', () => {
+    render(
+        <MockTodoFooter 
+          numberOfIncompleteTasks={5}
+        />
+    );
+    const pElement = screen.getByText(/5 tasks left/i);
+    expect(pElement).toBeInTheDocument();
+  });
+
+  it('should render "task" when the number of incomplete tasks is one', () => {
+    render(
+        <MockTodoFooter 
+          numberOfIncompleteTasks={1}
+        />
+    );
+    const pElement = screen.getByText(/1 task left/i);
+    expect(pElement).toBeInTheDocument();
+  });
+
+})

66ee9865e382d23a827be5fa567758b352d06938
Johannes Danusantoso <jdanusantoso@hotmail.com>
2023-02-23 10:00:59 -0600

    2nd fire event test successful

--- ReactTestingTutorial/react-testing-tutorial-fire_events/src/components/AddInput/___AddInput___/AddInput.test.js
+++ ReactTestingTutorial/react-testing-tutorial-fire_events/src/components/AddInput/___AddInput___/AddInput.test.js
@@ -39,4 +39,28 @@ describe("AddInput", () => {
         expect(inputElement.value).toBe("Go Grocery Shopping");
             
         });
+
+
+    /*Test 3
+    Making sure that there is an empty input after button is clicked*/
+    it ("should have empty input when add button is clicked", () => {
+        render(
+            <AddInput 
+                //get the todos from state in AddInput.js
+                todos={[]}
+                //passing the mockedSetTodo into the set todos
+                setTodos={mockedSetTodo}
+            />
+        );
+
+        const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+        const buttonElement = screen.getByRole("button", { name: /Add/i})
+        //Fire a change event
+        fireEvent.change(inputElement, { target: { value: "Go Grocery Shopping"}})
+        fireEvent.click(buttonElement)
+        //Expect the placeholder to be blank after clicking button
+        expect(inputElement.value).toBe("");
+            
+        });
+
 })

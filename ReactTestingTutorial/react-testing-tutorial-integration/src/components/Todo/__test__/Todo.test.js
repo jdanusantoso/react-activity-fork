ca4ca7cb65f280eef11fde5b9cf80bd78e99329b
Johannes Danusantoso <jdanusantoso@hotmail.com>
2023-02-25 23:42:24 -0600

    integration test completed

--- ReactTestingTutorial/react-testing-tutorial-integration/src/components/Todo/__test__/Todo.test.js
+++ ReactTestingTutorial/react-testing-tutorial-integration/src/components/Todo/__test__/Todo.test.js
@@ -13,18 +13,58 @@ const MockTodo = () => {
   )
 }
 
-/*TEST 1 Element Only*/ 
+//Adding each task that has inputted into the component through array
+const addTask = (tasks) => {
+  const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+  const buttonElement = screen.getByRole("button", { name: /Add/i} );
+  tasks.forEach((task) => {
+      fireEvent.change(inputElement, { target: { value: task } });
+      fireEvent.click(buttonElement);
+  })
+}
+
+/*TEST 1 Element*/ 
 
 describe("Todo", () => {
   test('should render same text passed into title prop', () => {
      render(< MockTodo/>);
-     const inputElement = screen.getAllByPlaceholderText(/Add a new task here.../i)
-     const buttonElement = screen.getAllByRole("button", {name: /Add/i});
-     fireEvent.change(inputElement, {target: { value: "Go Grocery Shopping"} } )
-     fireEvent.click(buttonElement)
+     addTask(["Go Grocery Shopping"])
      const divElement = screen.getByText(/Go Grocery Shopping/i)
      expect(divElement).toBeInTheDocument()
     });
-})
+
+/*TEST Multiple Elements*/ 
+
+
+it('should render multiple items', () => {
+    render(
+        <MockTodo />
+    );
+    addTask(["Go Grocery Shopping", "Go Grocery Shopping", "Go Grocery Shopping"])
+    const divElements = screen.queryAllByText(/Go Grocery Shopping/i);
+    expect(divElements.length).toBe(3)
+});
+
+it('task should not have complete class when initally rendered', () => {
+  render(
+      <MockTodo />
+  );
+  addTask(["Go Grocery Shopping"])
+  const divElement = screen.getByText(/Go Grocery Shopping/i);
+  expect(divElement).not.toHaveClass("todo-item-active")
+});
+
+it('task should have complete class when clicked', () => {
+  render(
+      <MockTodo />
+  );
+  addTask(["Go Grocery Shopping"])
+  const divElement = screen.getByText(/Go Grocery Shopping/i);
+  fireEvent.click(divElement)
+  expect(divElement).toHaveClass("todo-item-active")
+});
+
+
+});
  
-  
+ 

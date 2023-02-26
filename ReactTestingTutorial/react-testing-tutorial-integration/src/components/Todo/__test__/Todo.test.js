fc3840146bd0a161e2a248338026940516535ce4
Johannes Danusantoso <jdanusantoso@hotmail.com>
2023-02-25 23:09:46 -0600

    1 element interaction

--- ReactTestingTutorial/react-testing-tutorial-integration/src/components/Todo/__test__/Todo.test.js
+++ ReactTestingTutorial/react-testing-tutorial-integration/src/components/Todo/__test__/Todo.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import Todo from '../Todo';
 
@@ -13,15 +13,17 @@ const MockTodo = () => {
   )
 }
 
-/*TEST 1*/ 
-
-// FAILED TEST
+/*TEST 1 Element Only*/ 
 
 describe("Todo", () => {
   test('should render same text passed into title prop', () => {
-     render(< Todo/>);
-    //  const headingElement = screen.getByText(/learn react/i);
-    //  expect(headingElement).toBeInTheDocument();
+     render(< MockTodo/>);
+     const inputElement = screen.getAllByPlaceholderText(/Add a new task here.../i)
+     const buttonElement = screen.getAllByRole("button", {name: /Add/i});
+     fireEvent.change(inputElement, {target: { value: "Go Grocery Shopping"} } )
+     fireEvent.click(buttonElement)
+     const divElement = screen.getByText(/Go Grocery Shopping/i)
+     expect(divElement).toBeInTheDocument()
     });
 })
  

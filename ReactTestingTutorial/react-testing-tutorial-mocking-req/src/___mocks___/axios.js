fdf0f39893ea3ef3b21406068eac621bf6991103
Johannes Danusantoso <jdanusantoso@hotmail.com>
2023-02-26 15:26:05 -0600

    mock axios call set up

---
+++ ReactTestingTutorial/react-testing-tutorial-mocking-req/src/___mocks___/axios.js
@@ -0,0 +1,23 @@
+//Mocking a response
+const mockResponse = {
+    data: {
+        results: [
+            {
+                name: {
+                    first: "Laith",
+                    last: "Harb"
+                },
+                picture: {
+                    large: "https://randomuser.me/api/portraits/men/59.jpg"
+                },
+                login: {
+                    username: "ThePhonyGOAT"
+                }
+            }
+        ]
+    }
+}
+
+export default {
+    get: jest.fn().mockResolvedValue(mockResponse)
+}
